411f2e3e7f1e6e3c045e12318889c5268de87d23
kdm111 <pjsu94@naver.com>
2023-08-21 17:24:35 +0900

    study: dynamic formdata

--- 13-file-upload/app.js
+++ 13-file-upload/app.js
@@ -40,7 +40,7 @@ const uploadDetail = multer({
 // single("") : 하나의 파일을 업로드할 때 사용하는 메서드
 // 클라이언트의 요청이 들어오면 multer 설정(변수)에 따라 파일을 처리한 후 req.file 객체를 생성한다.
 // single 내부 인자는 input 태그의 name과 일치해야 한다.
-app.post("/upload", uploadDetail.single("userFile"), (req, res) => {
+app.post("user/upload", uploadDetail.single("userFile"), (req, res) => {
   // req.file에는 파일 업로드 정보가 들어있다.
   // 파일은 업로드 되지만 확장자
   console.log(req.file)
@@ -83,7 +83,7 @@ app.set("view engine", "ejs")
 // view 템플릿 파일이 모여있는 폴더 설정
 app.set("views", "./views")
 // css js 등 static 파일이 모여있는 폴더 설정
-app.use("/public", express.static(__dirname + "/static"))
+app.use("/user/uploads", express.static(__dirname + "/uploads"))
 
 
 // body-parser
@@ -110,6 +110,11 @@ const register = multer({
   })
 })
 
+app.post("/dynamic/file", uploadDetail.single("dynamicUserFile"), (req,res) => {
+  console.log(req.file)
+  console.log(req.body)
+  res.send(req.file)
+} )
 app.get("/", (req, res) => {
   res.render("index")
 })
