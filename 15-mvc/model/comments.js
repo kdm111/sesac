607c562f0edddec8d19b6ebb082a168441e1a85e
kdm111 <pjsu94@naver.com>
2023-08-25 18:43:52 +0900

    study: mvc 패턴

---
+++ 15-mvc/model/comments.js
@@ -0,0 +1,33 @@
+// model
+// db에서 전체 댓글 목록을 받아왔다고 가정
+// 모델과 db가 연동될 예정
+const comments = [
+  {
+    id: 1,
+    userid: 'helloworld',
+    date: '2022-10-31',
+    comment: '안녕하세요^~^',
+  },
+  {
+    id: 2,
+    userid: 'hroutery',
+    date: '2022-11-01',
+    comment: '반가워유',
+  },
+  {
+    id: 3,
+    userid: 'lucky',
+    date: '2022-11-02',
+    comment: '오 신기하군',
+  },
+  {
+    id: 4,
+    userid: 'bestpart',
+    date: '2022-11-02',
+    comment: '첫 댓글입니당ㅎㅎ',
+  },
+];
+
+exports.getCommentsAll = () => {
+  return comments;
+}
